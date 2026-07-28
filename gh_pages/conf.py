7b0a9339c4e024f53928fe6b861a1150f2cb4f15
dba <babicdd@gmail.com>
2026-07-28 20:48:54 +0800

    ico

--- gh_pages/conf.py
+++ gh_pages/conf.py
@@ -4,7 +4,7 @@ html_additional_pages = {
     'features': 'features.html',
     'stories': 'stories.html',
 }
-html_favicon = './favicon.ico'
+html_favicon = '_templates/favicon-32x32.png'
 templates_path = ['_templates']
 source_suffix = ['.txt']
 master_doc = 'contents'
